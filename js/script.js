3fd692e9df6817f9bb5b121371b3f68d75eb4c2e
Amr Adel Ata <amradelata@gmail.com>
2018-05-16 17:42:01 +0200

    media

--- js/script.js
+++ js/script.js
@@ -1,55 +1,30 @@
-// const app = document.querySelector('#app');
-// const cellSize = 85; // for performance issues we need to keep the cells pretty big!
-// const cols = Math.ceil(window.innerWidth / cellSize);
-// const rows = Math.ceil(window.innerHeight / cellSize);
 
-// // console.log(image);
+            var containerEl = document.querySelector('.container');
 
-// // Fill the Grid wil Cells
-// for (let i = 0; i < rows; i++) {
-//     const row = document.createElement('div');
-//     row.classList.add('row');
-//     for (let j = 0; j < cols; j++) {
-//         // Create each cell
-//         const el = document.createElement('div');
-//         el.classList.add('card');
-//         el.style.width = `${cellSize}px`;
-//         el.style.height = `${cellSize}px`;
-//         // Create the arrow image
-//         const image = document.createElement('img');
-//         image.src = 'https://image.flaticon.com/icons/svg/109/109617.svg';
-//         el.appendChild(image);
+            var mixer = mixitup(containerEl);
 
-//         // Append the new cell
-//         row.appendChild(el);
-//     }
-//     app.appendChild(row);
-// }
+            
 
-// const allCells = document.querySelectorAll('.card');
+	$(window).scroll(function () {
 
-// document.addEventListener('mousemove', e => {
-//     const currentPosition = { x: e.pageX, y: e.pageY };
+	// scrollTop button
 
-//     allCells.forEach(el => {
-//         const cellCoords = getElementPosition(el);
-//         const angle = getAngle(cellCoords, currentPosition);
-//         el.style.transform = `rotate(${angle}deg)`;
-//     });
-// });
+		let buttonTop = $('.scroll-top');
+		if($(window).scrollTop()>=2000){
+			buttonTop.fadeIn(500);
+			// console.log('dfds')
+		}else{
+			buttonTop.fadeOut(500);
+		}
+	});
 
-// function getElementPosition(el) {
-//     const elInfo = el.getBoundingClientRect();
-//     // We need to add half of the element width and height because getBoundingClientRect calculate form the edges
-//     const elHalfWidth = el.offsetWidth / 2;
-//     const elLocation = {
-//         x: elInfo.left + elHalfWidth,
-//         y: elInfo.top + elHalfWidth
-//     };
+			 $('.scroll-top').click(function () {
 
-//     return elLocation;
-// }
+			$('html, body').animate({
 
-// function getAngle(p1, p2) {
-//     return Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
-// }
+				scrollTop:0
+
+			},1000);
+
+			});
+
